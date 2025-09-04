73243586147387c0b1fae321ef479af3e5d37135
Menga wanji <martialwanji@gmail.com>
2025-09-04 19:10:13 +0100

    Initialise the server

--- Backend/server.js
+++ Backend/server.js
@@ -1,12 +1,18 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { createServer } from 'http';
 
 
 dotenv.config();
 
 const app = express();
+const server= createServer(app);
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.get('/', (req, res) =>{
+    res.send('<h1>Polling App</h1>');
+})
+
+server.listen(PORT, () => {
     console.log(`server running on ${PORT}`);
 });
