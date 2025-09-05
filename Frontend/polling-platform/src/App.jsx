5e916d7bd3801dd87cbdaa8dfaeab4b0f3ad3635
Ulrich Killian <fastfrightshipping@gmail.com>
2025-09-05 22:32:39 +0100

    updateapp.js

--- Frontend/polling-platform/src/App.jsx
+++ Frontend/polling-platform/src/App.jsx
@@ -1,12 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Header from './Components/Header/Header'
+import Polls from './Components/Polls/Polls'
+import Question from './Components/Question/Questions'
 
-import React from 'react'
 
-const App = () => {
-  return (
-    <div>
-
-    </div>
-  )
-}
+/* const App = () => (
+  <Router>
+    <Header />
+    <Routes>
+      <Route path="/" element={<div>Welcome Home</div>} />
+      <Route path="/polls" element={<Polls />} />
+      <Route path="/questions" element={<Question />} />
+    </Routes>
+  </Router>
+) */
 
 export default App
