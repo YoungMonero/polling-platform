992c1c0bdc8d6001057f8437f45ec3644585b682
youngmonero <yungmonero29@gmail.com>
2025-09-05 19:21:19 +0100

    inputs and inputvalidation

--- Frontend/polling-platform/src/App.jsx
+++ Frontend/polling-platform/src/App.jsx
@@ -1,10 +1,12 @@
 
 import React from 'react'
+import Participant from './components/participant/Participant'
+import Signup from './components/signup/signup'
 
 const App = () => {
   return (
     <div>
-      
+      <Signup/>
     </div>
   )
 }
